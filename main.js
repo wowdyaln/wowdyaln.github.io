a233add7df98b96cac9da1c26068022845188829
wowdyaln <rogersmk@gmail.com.tw>
2018-09-28 00:36:33 +0800

    [bug fixed] 短時間點擊多張卡片，會有卡片無法翻回來

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// todo: 偶爾會有單張卡片無法 recover，或是無法 flip 的 bug
+
 let firstCard = {
   isFliped: false,
   logo: "",
@@ -8,7 +10,10 @@ let secondCard = {
   logo: ""
 }
 
+let clickLock = false;
+
 document.addEventListener('click', (e)=> {
+  if (clickLock) { return }
   let dom = e.target.parentElement.classList
   if (dom.contains('pause')){ return }
   if (!firstCard.isFliped){
@@ -17,17 +22,19 @@ document.addEventListener('click', (e)=> {
 
   } else if (firstCard.isFliped && !secondCard.isFliped){
     flipCard(e)
+    clickLock = true  //!
     secondCard.logo = e.target.alt
     setTimeout( ()=>{
       if (secondCard.logo === firstCard.logo) {
         resetCardState()
         return
-
+        
       } else {
         // recover this card and a card which logo is : firstCard.logo
         secondCard.isFliped = false
         secondCard.logo = ""
         flipCard(e)
+        
         recoverPreviousCard(firstCard.logo)
         resetCardState()
       }
@@ -35,9 +42,7 @@ document.addEventListener('click', (e)=> {
   }
 })
 
-// todo: 短時間點擊多張卡片，會有卡片無法翻回來的 bug
 function flipCard(e){
-  console.log("clicked");
   let dom = e.target.parentElement.classList
   if (dom.contains('memory__card')){
     dom.toggle('flip')
@@ -55,6 +60,7 @@ function checkFirstCard(e){
 }
 
 function resetCardState(){
+  clickLock = false
   firstCard.isFliped = false
   firstCard.isSun = false
   secondCard.isFliped = false
