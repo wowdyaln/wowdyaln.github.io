fddbe48979ae6ecee430aba0f7a589ac7c75d932
wowdyaln <rogersmk@gmail.com.tw>
2018-09-27 13:11:07 +0800

    if second flip is worong, recover second_card.

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
+let firstCard = {
+  isFliped: false,
+  logo: ""
+}
+let secondCard = {
+  isFliped: false,
+  logo: ""
+}
+
 document.addEventListener('click', (e)=> {
-  flipCard(e)
+  let dom = e.target.parentElement.classList
+  if (dom.contains('pause')){ return }
+  if (!firstCard.isFliped){
+    checkFirstCard(e)
+    flipCard(e)
+  }else if (firstCard.isFliped && !secondCard.isFliped){
+    
+    flipCard(e)
+    // secondCard.isFliped = true
+      secondCard.logo = e.target.alt
+    
+    setTimeout( ()=>{
+
+      if (secondCard.logo === firstCard.logo) {
+        resetCardState()
+        return
+      } else {
+        // recover this card and a card which logo is : firstCard.logo
+        secondCard.isFliped = false
+        secondCard.logo = ""
+        flipCard(e)
+
+        // resetCardState()
+      }
+
+    }, 1000) // hardcode , 怎麽直接取得 CSS .memory__card transition 的秒數？
+    
+  }
 })
 
 
@@ -9,7 +45,22 @@ function flipCard(e){
   let dom = e.target.parentElement.classList
   if (dom.contains('memory__card')){
     dom.toggle('flip')
+    dom.toggle('pause')
   }
-  // e.target.parentElement.classList.toggle("flip")
-  // e.target.parentElement.classList.toggle("active")
 }
+
+// first flip ; second flip
+// after flip, check 'alt' property
+function checkFirstCard(e){
+  // let dom = e.target.parentElement.classList
+    firstCard.isFliped = true
+    firstCard.logo = e.target.alt
+}
+
+function resetCardState(){
+  firstCard.isFliped = false
+  secondCard.isFliped = false
+  firstCard.logo = ""
+  secondCard.logo = ""
+}
+    
