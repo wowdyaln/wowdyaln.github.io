3887a8ee33409b93eaf843f2a2ac3d8496c12e09
wowdyaln <rogersmk@gmail.com.tw>
2018-10-01 17:43:01 +0800

    tweet favs list 切版/ api lib: codebird

---
+++ twitFav/script/auth.js
@@ -0,0 +1,11 @@
+/* //! 有點複雜才能生成符合 OAuth 1.0 的 request header
+ ref: 
+ https://developer.twitter.com/en/docs/basics/authentication/guides/authorizing-a-request
+ https://imagineer.in/blog/authorizing-twitter-api-calls-in-javascript/
+
+*/
+
+let  oauth_consumer_key = "oEfQb1R21bnxoDL4JthwxP2XB",
+oauth_secret = "F4wOq9bxi9xdVQ67kTslDCHnHO3XnnYRjSb5o3ZhurBmunsCgj",
+oauth_token = "166235026-Gz4vTZ0JnS7RSAKJ7fvJ2GlCCaX5t2g3egWw8Dj3",
+oauth_token_secret = "QyhYf4P8JnEdgiVVaYlbcQ2wXOqGss7lj5aUDYZ3xdfvs",
---
+++ twitFav/script/listFav.js
@@ -0,0 +1,20 @@
+var cb = new Codebird;
+cb.setConsumerKey("oEfQb1R21bnxoDL4JthwxP2XB", "F4wOq9bxi9xdVQ67kTslDCHnHO3XnnYRjSb5o3ZhurBmunsCgj");
+cb.setToken("166235026-Gz4vTZ0JnS7RSAKJ7fvJ2GlCCaX5t2g3egWw8Dj3", "QyhYf4P8JnEdgiVVaYlbcQ2wXOqGss7lj5aUDYZ3xdfvs");
+
+
+
+let params = {
+  count: 5,
+  screen_name: "poooo_chu"
+}
+
+// cb.__call("favorites/list", params, function (reply, rate, err) {
+//   reply.forEach(tweet => {
+//     console.log(tweet);
+//   })
+//   // console.log(reply);
+//   console.log(err);
+// });
+
+
