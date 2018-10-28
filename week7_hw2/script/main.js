2add8563f75d065635e9bd42a271b0318bdf91b2
wowdyaln <rogersmk@gmail.com.tw>
2018-10-29 00:15:24 +0800

    week10-3

--- week7_hw2/script/main.js
+++ week7_hw2/script/main.js
@@ -2,61 +2,72 @@
 
 $(document).ready( function(){
   console.log("jquery ready")
+  let lists = []
 
-  function createTodoEle(content){
-    let todoRow = `
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-            ${content}
-            <p class="mr-auto"></p>
-            <button type="button" class="btn__delete btn btn-outline-secondary p-2 mr-3">DELETE</button>
-            <button type="button" class="btn__finish btn btn-outline-success p-2">Finish</button>
-      </li>
-      `
-    $('.list-group').prepend(todoRow)
+  function addTodo(content) {
+    let todo = {content,
+                isDone: false}
+    lists.unshift(todo)
+    render()
     $('input').val("")
   }
 
-  function removeTodoEle(dom){
-    dom.parent().addClass('removeMe')
-    dom.parent().fadeOut(300, ()=>{
-      $('.removeMe').remove()
+  function changeState(content){
+    lists.forEach( list => {
+      if (list.content === content) {
+        if (list.isDone) {
+          list.isDone = false
+        } else {
+          list.isDone = true
+        }
+      }
+    })
+    render()
+  }
+
+  function removeTodo(content) {
+    lists = lists.filter( list => list.content !== content )
+    render()
+  }
+
+  function render() {
+    $('.list-group').empty()
+
+    lists.forEach( list => {
+      if (list.isDone) {
+        $('.list-group').append(`<li class="list-group-item d-flex justify-content-between align-items-center bg-secondary">
+                                      <p>${list.content}</p>
+                                      <p class="mr-auto"></p>
+                                      <button type="button" class="btn__delete btn p-2 mr-3 btn-outline-primary">DELETE</button>
+                                      <button type="button" class="btn__finish btn p-2 btn-outline-warning">REDO</button>
+                                </li>`)
+      } else {
+        $('.list-group').append(`<li class="list-group-item d-flex justify-content-between align-items-center">
+                                        <p>${list.content}</p>
+                                        <p class="mr-auto"></p>
+                                        <button type="button" class="btn__delete btn btn-outline-secondary p-2 mr-3">DELETE</button>
+                                        <button type="button" class="btn__finish btn btn-outline-success p-2">Finish</button>
+                                  </li>`)
+      }
     })
   }
 
-  function changeState(dom){
-    dom.parent().toggleClass('bg-secondary')
-    dom.prev().toggleClass('btn-outline-secondary').toggleClass('btn-outline-primary')
-    dom.toggleClass('btn-outline-success').toggleClass('btn-outline-warning')
-    if (dom.text() === "Finish"){
-      dom.text("REDO")
-    } else if (dom.text() === "REDO"){
-      dom.text("Finish")
-    }
-  }
-
-  // create a todo
   $('input').keyup( (e)=> {
-    // if press 'Enter'
-    if( e.which === 13) 
-    {
+    if( e.which === 13) {
       let todoContent = $('input').val()
-      createTodoEle(todoContent)
-    }
-  })
-
-  // remove a todo.
-  $('.list-group').click( (e)=> {
-    if ($(e.target).hasClass('btn__delete')){
-      removeTodoEle( $(e.target) )
-    }
-  })
-
-  // change state.
-  $('.list-group').click((e) => {
-    if ($(e.target).hasClass('btn__finish') || $(e.target).hasClass('btn__redo')) {
-      console.log('finished');
-      changeState( $(e.target) )
+      addTodo(todoContent)
     }
   })
   
+  $('.list-group').click( (e)=> {
+    // remove a todo.
+    if ($(e.target).hasClass('btn__delete')){
+      removeTodo( $(e.target).siblings()[0].innerText )
+    }
+    // change state.
+    if ($(e.target).hasClass('btn__finish') || $(e.target).hasClass('btn__redo')) {
+      changeState($(e.target).siblings()[0].innerText)
+    }
+  })
+
 })
